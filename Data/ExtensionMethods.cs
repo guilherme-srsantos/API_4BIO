2205465dd980e6c9d34d3c9cdb6fa4c1ad39b035
Guilherme Santos <guilherme.santos@seconds.com.br>
2023-03-01 00:24:56 -0300

    create custom extension methods to generate random RG numbers

---
+++ Data/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using System;
+
+namespace API_4BIO.Data
+{
+    public static class ExtensionMethods
+    {
+        private static readonly Random _random = new ();
+     
+        private static int Rng()
+        {
+            return _random.Next(0, 9);
+        }
+        public static string Rg(this Person person)
+        {
+            return $"{Rng()}{Rng()}.{Rng()}{Rng()}{Rng()}.{Rng()}{Rng()}{Rng()}-{Rng()}";
+        }
+
+    }
+}
