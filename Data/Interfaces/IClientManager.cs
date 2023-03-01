a6c33995dbf8f6f4f33992327c7ac41cf342493f
Guilherme Santos <guilherme.santos@seconds.com.br>
2023-03-01 00:23:11 -0300

    create interfaces for data handling

---
+++ Data/Interfaces/IClientManager.cs
@@ -0,0 +1,14 @@
+﻿using API_4BIO.Models;
+
+namespace API_4BIO.Data.Interfaces
+{
+    public interface IClientManager
+    {
+        Task<List<Client>> AddMultipleClientsAsync(List<Client> clients, CancellationToken ct);
+        Task<Client> CreateClientAsync(Client client, CancellationToken ct);
+        Task<bool> DeleteClientAsync(string clientId, CancellationToken ct);
+        Task<List<Client>> FindClientAsync(Filter filter, CancellationToken ct);
+        Task<List<Guid>> ImportDataFromJsonAsync(IFormFile file, CancellationToken ct);
+        Task<Client?> UpdateClientAsync(Client client, CancellationToken ct);
+    }
+}
---
+++ Data/Interfaces/IDataMocker.cs
@@ -0,0 +1,9 @@
+﻿using API_4BIO.Models;
+
+namespace API_4BIO.Data.Interfaces
+{
+    public interface IDataMocker
+    {
+        List<Client> GenerateMockClients(int amount);
+    }
+}
