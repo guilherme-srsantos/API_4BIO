8feab3c22af903eb929e39e32c7842d456736f26
Guilherme Santos <guilherme.santos@seconds.com.br>
2023-03-01 00:24:26 -0300

    define dependencies to be injected

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
 using FastEndpoints.Swagger;
 using FastEndpoints;
+using API_4BIO.Data;
+using API_4BIO.Data.Interfaces;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
 builder.Services.AddSwaggerDoc();
-
+builder.Services.AddDbContext<ApplicationDB>();
+builder.Services.AddScoped<IClientManager, ClientManager>();
+builder.Services.AddScoped<IDataMocker, DataMocker>();
 
 
 var app = builder.Build();
 
+
+app.UseDefaultExceptionHandler();
 app.UseHttpsRedirection();
 app.UseFastEndpoints();
 app.UseSwaggerGen();
