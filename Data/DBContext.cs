571af08db7f762c6567c2b86b824621600859946
Guilherme Santos <guilherme.santos@seconds.com.br>
2023-03-01 00:24:00 -0300

    add in memory db instance for testing

---
+++ Data/DBContext.cs
@@ -0,0 +1,35 @@
+﻿using API_4BIO.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_4BIO.Data
+{
+    public class ApplicationDB : DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<ContactInfo> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "ApiDb"); 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>()
+                .HasOne(c => c.Address)
+                .WithOne(c => c.Client)
+                .HasForeignKey<Address>(a => a.ClientId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            modelBuilder.Entity<Client>()
+                .HasOne(c => c.ContactInfo)
+                .WithOne(c => c.Client)
+                .HasForeignKey<ContactInfo>(a => a.ClientId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+
+    }
+}
